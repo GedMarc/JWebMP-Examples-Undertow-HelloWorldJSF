217a379de0c51156743fec7b886b42887ff6da14
GedMarc <ged_marc@hotmail.com>
2020-05-23 22:58:45 +0200

    Jiggy Jiggy Slim Shady.  Servlet 4  Undertow 2 Move most of everything to shades,

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -2,7 +2,7 @@ module com.jwebmp.examples.undertow.helloworld.jsf {
 
 	exports com.jwebmp.examples.undertow.helloworld to com.google.guice;
 
-	requires javax.servlet.api;
+	requires java.servlet;
 	requires com.guicedee.guicedservlets.jsf;
 
 	requires java.logging;
